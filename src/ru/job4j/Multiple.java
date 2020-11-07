021c95d80685a2f9c9f93ffd45e255a8698d855b
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2020-11-07 20:23:35 +0300

    3.3. Разбор первой программы. [#404342]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args){
+        int a, b;
+        a = 1;
+        b = 2;
+
+        while(b < 10){
+            System.out.printf("%d * %d = %d\n", a, b, a*b);
+            b++;
+        }
+    }
+}
---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Vasiliy Krasov");
+        System.out.println("07.11.2020");
+    }
+}
