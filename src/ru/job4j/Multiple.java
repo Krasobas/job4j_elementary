041340c1ed3287199770f9b0523a80284139b56f
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2020-11-09 11:56:03 +0300

    9.2. Checkstyle [#404338]

--- src/ru/job4j/Multiple.java
+++ src/ru/job4j/Multiple.java
@@ -1,13 +1,13 @@
 package ru.job4j;
 
 public class Multiple {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int a, b;
         a = 1;
         b = 2;
 
-        while(b < 10){
-            System.out.printf("%d * %d = %d\n", a, b, a*b);
+        while (b < 10) {
+            System.out.printf("%d * %d = %d\n", a, b, a * b);
             b++;
         }
     }
--- src/ru/job4j/StudentInfo.java
+++ src/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Vasiliy Krasov");
         System.out.println("07.11.2020");
     }
