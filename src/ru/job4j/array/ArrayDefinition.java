f1f28f01e9a3be575daab8de9b07db5125e22b93
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2021-01-13 18:08:06 +0300

    6.0.2. Заполнение массива. [#404303]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,7 +4,17 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         Short[] ages = new Short[10];
         String[] surnames = new String[100500];
+        String[] names = new String[4];
         Float[] prices = new Float[40];
+
+        names[0] = "Вера";
+        names[1] = "Надежда";
+        names[2] = "Любовь";
+        names[3] = "София";
+
+        for (int index = 0; index < names.length; index++) {
+            System.out.println(names[index]);
+        }
 
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
