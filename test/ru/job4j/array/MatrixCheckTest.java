f8354da93a60c18371c4e9f17500425526d73c16
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2021-01-16 13:36:34 +0300

    6.6.8. Двухмерный массив. Таблица умножения. [#404289]

---
+++ test/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MatrixCheckTest {
+
+    @Test
+    public void whenHasMonoHorizontal() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+        };
+        boolean result = MatrixCheck.monoHorizontal(input, 1);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasNotMonoHorizontal() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'Y', 'X', 'X'},
+                {' ', ' ', ' '},
+        };
+        boolean result = MatrixCheck.monoHorizontal(input, 1);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenHasMonoHorizontalInAnotherRow() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+        };
+        boolean result = MatrixCheck.monoHorizontal(input, 2);
+        assertThat(result, is(false));
+    }
+}
