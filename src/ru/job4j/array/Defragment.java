4ea2c05ce2f7dde11f6eedb7f65baf8cefee1093
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2021-01-21 07:11:06 +0300

    6.8. Дефрагментация массива [#404317]

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int nullIndex = index;
+                for (int notNullIndex = nullIndex; notNullIndex < array.length; notNullIndex++) {
+                    if (array[notNullIndex] != null) {
+                        array = SwitchArray.swap(array, nullIndex, notNullIndex);
+                        nullIndex++;
+                    }
+                }
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+}
