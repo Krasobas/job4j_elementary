8e7d0a3e4466fc4728d2f7647c1aa12353911547
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2021-02-12 09:44:28 +0300

    1. Экзамен. Базовый синтаксис. [#404366]

---
+++ src/ru/job4j/array/Merge.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+public class Merge {
+    public static int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int index = 0;
+        int x = 0;
+        int y = 0;
+
+        while (index < rsl.length) {
+            if (x < left.length && left[x] < right[y]) {
+                rsl[index] = left[x];
+                index++;
+                x++;
+            } else {
+                rsl[index] = right[y];
+                index++;
+                y++;
+            }
+        }
+
+        return rsl;
+    }
+}
